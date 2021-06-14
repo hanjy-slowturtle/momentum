9e25b38c89e50d4913859bf98f990dac530885c5
hanjy-slowturtle <h920915@gmail.com>
2021-06-14 22:13:43 +0900

    ~ #2.3 const and let

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const a = 5;
 const b = 2;
-const myName = "hanjy";
+let myName = "hanjy";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello " + myName);
+
+myName = "han jongyeop";
+
+console.log("your new name is " + myName);
