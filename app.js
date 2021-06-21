b7238644a68152b9e9aed52331aae62b82458da9
hanjy-slowturtle <h920915@gmail.com>
2021-06-21 23:24:13 +0900

    ~ #2.13 Conditionals

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-const calculator = {
-    add: function(a, b) {
-        console.log("Hello");
-        return a + b;
-        console.log("Bye bye");
-    },
-    minus: function(a, b) {
-        return a - b;
-    },
-    multiply: function(a, b) {
-        return a * b;
-    },
-    divide: function(a, b) {
-        return a / b;
-    }
-};
+const age = parseInt(prompt("How old are you?"));
 
-const addResult = calculator.add(2, 3);
-const minusResult = calculator.minus(addResult, 10);
-const multiplyResult = calculator.multiply(5, minusResult);
-const divideResult = calculator.divide(addResult, multiplyResult);
+console.log(age);
