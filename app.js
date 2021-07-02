3261a9fe152e20360198651d3bd60462401c433a
hanjy-slowturtle <h920915@gmail.com>
2021-07-03 02:13:02 +0900

    ~ #3.7 CSS in Javascript part Two

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const h1 = document.querySelector("div.hello:first-child h1");
 console.log(h1);
 
 function handleH1Click() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked";
+    if(h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleH1Click);
