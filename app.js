38fb19a6f6f1bfb0b419b0256188d94a1c44da58
hanjy-slowturtle <h920915@gmail.com>
2021-07-08 21:48:05 +0900

    ~ #4.3 Events part Two

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     console.log(event);
@@ -8,4 +10,10 @@ function onLoginSubmit(event) {
     console.log(username);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
