0beffd45fdee564c350fd96df909139cd4bdc207
hanjy-slowturtle <h920915@gmail.com>
2021-07-13 02:16:11 +0900

    ~ #4.7 Super Recap

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const USERNAME_KEY = "username";
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username);
-    printGreetings(username);
+    const typedUsername = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, typedUsername);
+    printGreetings(typedUsername);
 }
 
 function printGreetings(username) {
