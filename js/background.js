eb4910108b862aea1292c1df7534ab496694d92e
hanjy-slowturtle <h920915@gmail.com>
2021-07-18 22:08:05 +0900

    ~ #6.1 Background

---
+++ js/background.js
@@ -0,0 +1,11 @@
+const images = [
+    "01.jpg",
+    "02.jpg",
+    "03.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const img = document.createElement("img");
+img.src = `img/${chosenImage}`;
+document.body.appendChild(img);
