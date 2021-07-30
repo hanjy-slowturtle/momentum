fbf9c2f29cb46a0012eb571301f8e0bd41513d4f
hanjy-slowturtle <h920915@gmail.com>
2021-07-30 21:32:22 +0900

    ~ #8.2 Conclusions

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = '2786fc5e0dbc3157a5e44e316caf75cb';
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const log = position.coords.longitude;
-    console.log(lat, log);
+    const lon = position.coords.longitude;
+    console.log(lat, lon);
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const weatherSpanArr = document.querySelectorAll("#weather span");
+            weatherSpanArr[0].innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            weatherSpanArr[1].innerText = data.name;
+        });
 }
 
 function onGeoError() {
